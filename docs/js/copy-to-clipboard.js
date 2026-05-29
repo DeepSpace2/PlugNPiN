37bf069d79bef0812a1f5650097e48adf2a1ec7d
Adi <6841988+DeepSpace2@users.noreply.github.com>
2026-05-29 23:11:07 +0300

    making the copy to clipboard button more accessible

--- docs/js/copy-to-clipboard.js
+++ docs/js/copy-to-clipboard.js
@@ -17,6 +17,9 @@ document$.subscribe(function() {
     wrapper.appendChild(code);
     
     const button = document.createElement("button");
+    button.type = "button";
+    button.setAttribute("aria-label", "Copy to clipboard");
+    button.setAttribute("title", "Copy to clipboard");
     button.classList.add("copy-to-clipboard-button");
     button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 21H8V7h11m0-2H8a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2m-3-4H4a2 2 0 0 0-2 2v14h2V3h12V1Z"/></svg>';
     
